90843cd2fecea1b389a9e24734d8bdba108f3c9d
Pierre GRANGER <pgranger23@gmail.com>
2025-02-04 23:19:32 +0100

    Forgot this file...

--- src/content/config.ts
+++ src/content/config.ts
@@ -34,10 +34,29 @@ const teamCollection = defineCollection({
   }),
 });
 
+const experimentCollection = defineCollection({
+  schema: z.object({
+    draft: z.boolean(),
+    name: z.string(),
+    short_description: z.string(),
+    description: z.string(),
+    publishDate: z.string().transform(str => new Date(str)),
+    image1: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    image2: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'blog': blogCollection,
   'team': teamCollection,
   'visitors': teamCollection,
+  'experiments': experimentCollection,
 };
